0b9ccfab0213d3f178a85fb126745170ba6f5203
Ritsuki Goto <develop@mizucoffee.net>
2021-01-31 23:26:00 +0900

    [add] profile

--- js/anim.js
+++ js/anim.js
@@ -3,4 +3,10 @@ window.addEventListener('DOMContentLoaded', () => {
     path.style.strokeDasharray = path.getTotalLength()
     path.style.strokeDashoffset = path.getTotalLength()
   })
+  
+  window.addEventListener('scroll', () => {
+    if(window.innerHeight/4*3 < window.scrollY) {
+      document.querySelector('.standing').classList.add('show')
+    }
+  })
 })
