5db6d8bedf4fff9664f440831794de8d7ae4aa00
mizucoffee <develop@mizucoffee.net>
2021-02-15 01:05:11 +0900

    [update] contents

--- js/anim.js
+++ js/anim.js
@@ -6,4 +6,33 @@ window.addEventListener('DOMContentLoaded', () => {
   })
 　
   document.querySelector('.bg').style.backgroundImage = `url(/image/${images[Math.floor(Math.random()*images.length)]}.webp)`;
+  
+  document.querySelector('#photo-btn').addEventListener('click',() => {
+    document.querySelector('#main').classList.add('hidden')
+    document.querySelector('#photo').classList.remove('hidden')
+  })
+  document.querySelector('#photo-back-btn').addEventListener('click',() => {
+    document.querySelector('#main').classList.remove('hidden')
+    document.querySelector('#photo').classList.add('hidden')
+  })
+  
+  document.querySelector('#about-btn').addEventListener('click',() => {
+    document.querySelector('#main').classList.add('hidden')
+    document.querySelector('#about').classList.remove('hidden')
+  })
+  document.querySelector('#about-back-btn').addEventListener('click',() => {
+    document.querySelector('#main').classList.remove('hidden')
+    document.querySelector('#about').classList.add('hidden')
+  })
+  
+  document.querySelector('#contact-btn').addEventListener('click',() => {
+    document.querySelector('#main').classList.add('hidden')
+    document.querySelector('#contact').classList.remove('hidden')
+  })
+  document.querySelector('#contact-back-btn').addEventListener('click',() => {
+    document.querySelector('#main').classList.remove('hidden')
+    document.querySelector('#contact').classList.add('hidden')
+  })
+  
+  photo-back-btn
 })
